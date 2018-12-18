06b9f9c01d121db0b4fe5b0324263b1aebb5c364
Iglan Cardeal <cmtcardeal@outlook.com>
2018-12-17 23:22:20 -0300

    Adicionando um focus

--- src/components/inputText/InputText.js
+++ src/components/inputText/InputText.js
@@ -3,18 +3,24 @@ import Wrapper from '../../hoc/wrapper';
 import classes from '../../hoc/classes';
 import style from './InputText.module.css';
 
-const inputText = props => {
-  return (
-    <Wrapper>
-      <p>
-        Você pode escrever e ver abaixo o texto original e o texto criptografado. Não utilize caracteres acentuados ou especiais no seu texto.
-      </p>
-      <p>
-        <label htmlFor="input-text">Digite algo:</label>
-        <input type="text" id="input-text" onChange={ props.change }/>
-      </p>
-    </Wrapper>
-  )
+class InputText extends React.Component {
+  componentDidMount() {
+    this.focusOnElement.focus();
+  }
+
+  render() {
+    return (
+      <Wrapper>
+        <p>
+          Você pode escrever e ver abaixo o texto original e o texto criptografado. Não utilize caracteres acentuados ou especiais no seu texto.
+        </p>
+        <p>
+          <label htmlFor="input-text">Digite algo:</label>
+          <input ref={element => {this.focusOnElement = element}} type="text" id="input-text" onChange={ this.props.change }/>
+        </p>
+      </Wrapper>
+    )
+  }
 }
 
-export default classes(inputText, style.InputText);
+export default classes(InputText, style.InputText);
