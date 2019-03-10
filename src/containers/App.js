3011820c8b468d77da8ad9874894183c5c735684
Iglan Cardeal <cmtcardeal@outlook.com>
2019-03-10 03:22:45 -0300

    Adicionando descricao e removendo arquivos e codigos standart do CRA

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,42 +1,50 @@
-import React, { Component } from 'react';
-import Wrapper from '../hoc/wrapper';
-import classes from '../hoc/classes';
-import style from './App.module.css';
+/**
+  * @cifradecesarApp
+  * @author Iglan Cardeal
+  * @githubrepository https://github.com/IglanCardeal/cifra-de-cesar
+  * Aplicacao que criptografa um texto digitado pelo usuario baseado no conceito da cifra de Cesar.
+  */
+
+import React, { Component } from "react";
+import Wrapper from "../hoc/wrapper";
+import classes from "../hoc/classes";
+import style from "./App.module.css";
+
 // Componentes
-import  InputText  from '../components/inputText/InputText';
-import  OutputText  from '../components/outputText/OutputText';
-import  OriginalText  from '../components/originalText/OriginalText';
-import  Main  from '../components/main/Main';
+import InputText from "../components/inputText/InputText";
+import OutputText from "../components/outputText/OutputText";
+import OriginalText from "../components/originalText/OriginalText";
+import Main from "../components/main/Main";
 
 class App extends Component {
   state = {
-    text: '',
-  }
+    text: ""
+  };
 
   inputTextHandler = event => {
     let newText = event.target.value.toLowerCase();
     this.setState(() => {
       return {
-        text: newText,
-      }
-    })
-  }
+        text: newText
+      };
+    });
+  };
 
   hasText = () => {
     let showOriginalText;
     let showOutputText;
     let textLength = this.state.text.length > 0 ? true : false;
-    if(textLength){
-      showOriginalText = <OriginalText text={this.state.text} />
-      showOutputText = <OutputText text={this.state.text} />
+    if (textLength) {
+      showOriginalText = <OriginalText text={this.state.text} />;
+      showOutputText = <OutputText text={this.state.text} />;
     }
     return (
       <Wrapper>
         {showOriginalText}
         {showOutputText}
       </Wrapper>
-    )
-  }
+    );
+  };
 
   render() {
     return (
